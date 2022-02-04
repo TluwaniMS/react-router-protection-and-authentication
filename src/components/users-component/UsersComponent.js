ca85ec4bb1742f1162acf7bdb825b66718cf972d
tluwaniMS <tluwani2020@gmail.com>
2022-02-04 16:57:10 +0200

    adding-users-display

--- src/components/users-component/UsersComponent.js
+++ src/components/users-component/UsersComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AdminLayoutComponent from '../admin-layout-component/AdminLayoutComponent'
 import SpinnerDisplay from '../../displays/SpinnerDisplay/SpinnerDisplay'
+import UsersDisplay from '../../displays/UsersDisplay/UsersDisplay'
 
 const UsersComponent = () => {
     return <AdminLayoutComponent></AdminLayoutComponent>
---
+++ src/displays/UsersDisplay/UsersDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const UsersDisplay = () => {
+    return <div></div>
+}
+
+export default UsersDisplay
