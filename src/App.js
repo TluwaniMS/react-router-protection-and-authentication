bbd2934d5dbf81032817f82f6c8ddd032cd9d260
tluwaniMS <tluwani2020@gmail.com>
2022-02-04 15:35:11 +0200

    adding-home-route-and-sign-in

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import MunicipalitiesComponent from './components/municipalities-component/Munic
 import DoctorsComponent from './components/doctors-component/DoctorsComponent'
 import HospitalsComponent from './components/hospitals-component/HospitalsComponent'
 import UsersComponent from './components/users-component/UsersComponent'
+import HomeComponent from './components/home-component/HomeComponent'
+import SignInComponent from './components/sign-in-componet/SignInComponent'
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                <Route path="/" element={<HomeComponent />} />
+                <Route path="/sign-in" element={<SignInComponent />} />
                 <Route path="/users" element={<UsersComponent />} />
                 <Route path="/municipalities" element={<MunicipalitiesComponent />} />
                 <Route path="/hospitals" element={<HospitalsComponent />} />
