c704563f668d24676ee279097995eaa5eb37b1db
tluwaniMS <tluwani2020@gmail.com>
2022-02-08 21:53:24 +0200

    adding-user-sign-in-form-and-graphql-mutation

--- src/components/sign-in-componet/SignInComponent.js
+++ src/components/sign-in-componet/SignInComponent.js
@@ -1,12 +1,59 @@
 import React from 'react'
+import * as yup from 'yup'
 import { useMutation } from '@apollo/client'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
 import SpinnerDisplay from '../../displays/SpinnerDisplay/SpinnerDisplay'
 import LayoutComponent from '../layout-component/LayoutComponent'
+import { AUTHENTICATION } from '../../mutations/authentication.mutation'
+import './SignInComponent.css'
 
 const SignInComponent = () => {
+    const [authenticateUserWithPassword, { data, loading, error }] = useMutation(AUTHENTICATION)
+
+    const schema = yup.object({
+        email: yup.string().email('Please enter a valid email').required('Email is required'),
+        password: yup.string().required('Password is required')
+    })
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm({ resolver: yupResolver(schema) })
+
+    const resetFormFields = () => reset()
+
+    const login = data => {
+        authenticateUserWithPassword({ variables: { email: data.email, password: data.password } })
+        reset()
+    }
+
     return (
         <LayoutComponent>
-            <div>Sign in view works!!</div>
+            <div className="main-login-form-container">
+                <div className="login-form-container">
+                    <form onSubmit={handleSubmit(login)} className="form-input-container">
+                        <div className="input-container">
+                            <input placeholder="email" className="input" {...register('email')}></input>
+                            {errors.email?.message}
+                        </div>
+                        <div className="input-container">
+                            <input
+                                placeholder="password"
+                                type="password"
+                                className="input"
+                                {...register('password')}
+                            ></input>
+                            {errors.password?.message}
+                        </div>
+                        <div className="login-button-container">
+                            <button type="submit">submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </LayoutComponent>
     )
 }
--- src/mutations/authentication.mutation.js
+++ src/mutations/authentication.mutation.js
@@ -1,3 +1,19 @@
 import { gql } from '@apollo/client'
 
-export const AUTHENTICATION = gql``
+export const AUTHENTICATION = gql`
+    mutation AuthenticateUserWithPassword($email: String!, $password: String!) {
+        authenticateUserWithPassword(email: $email, password: $password) {
+            ... on UserAuthenticationWithPasswordSuccess {
+                sessionToken
+                item {
+                    name
+                    isAdmin
+                    email
+                }
+            }
+            ... on UserAuthenticationWithPasswordFailure {
+                message
+            }
+        }
+    }
+`
