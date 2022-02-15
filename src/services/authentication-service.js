d919aec7a27a6251756450b9375f753d7c5e71e6
tluwaniMS <tluwani2020@gmail.com>
2022-02-15 20:51:57 +0200

    adding-logic-to-check-if-session-is-in-progress

--- src/services/authentication-service.js
+++ src/services/authentication-service.js
@@ -1,11 +1,13 @@
 export const signOut = () => {
     sessionStorage.removeItem('sessionToken')
     sessionStorage.removeItem('userInformation')
+    sessionStorage.removeItem('sessionIsActive')
 }
 
 export const setSessionInformation = (sessionToken, userSessionData) => {
     sessionStorage.setItem('sessionToken', sessionToken)
     sessionStorage.setItem('userInformation', userSessionData)
+    sessionStorage.setItem('sessionIsActive', true)
 }
 
 export const getUserData = () => {
@@ -18,4 +20,10 @@ export const getSessionToken = () => {
     const sessionToken = sessionStorage.getItem('sessionToken')
 
     return sessionToken
+}
+
+export const checkIfSessionIsActive = () => {
+    const sessionIsActive = sessionStorage.getItem('sessionIsActive')
+
+    return sessionIsActive
 }
